b3bb6c9470cb24890f76da8b978b42bf08ef2dc5
Mbugua Mwaura <mbuguamwaura9@gmail.com>
2019-05-19 18:42:53 +0300

    adds a test to check is the hero object instantiates with the power

--- src/main/java/Hero.java
+++ src/main/java/Hero.java
@@ -1,15 +1,20 @@
 public class Hero{
     private String mName;
     private int mAge;
+    private String mPower;
 
-    public Hero(String name, int age){
+    public Hero(String name, int age, String power){
         mName = name;
         mAge = age;
+        mPower = power;
     }
     public String getName(){
         return mName;
     }
     public int getAge(){
         return mAge;
+    }
+    public String getPower(){
+        return mPower;
     }
 }
--- src/test/java/HeroTest.java
+++ src/test/java/HeroTest.java
@@ -4,19 +4,25 @@ import static org.junit.Assert.*;
 public class HeroTest{
     @Test
     public void Hero_instantiatesCorrectly_true(){
-        Hero heroOne = new Hero("Superman",30);
+        Hero heroOne = new Hero("Superman",30,"Flight");
         assertEquals(true, heroOne instanceof Hero);
     }
 
     @Test
     public void Hero_instantiatesWithName_String(){
-        Hero heroTest = new Hero("Superman",30);
+        Hero heroTest = new Hero("Superman",30, "Flight");
         assertEquals("Superman", heroTest.getName());
     }
 
     @Test
     public void Hero_instantiatesWithAge_Int(){
-        Hero heroTest = new Hero("Superman", 30);
+        Hero heroTest = new Hero("Superman", 30, "Flight");
         assertEquals(30, heroTest.getAge());
+    }
+
+    @Test
+    public void Hero_instantiatesWithPower_String(){
+        Hero heroTest = new Hero("Superman", 30, "Flight");
+        assertEquals("Flight", heroTest.getPower());
     }
 }
