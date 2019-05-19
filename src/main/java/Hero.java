bd18332945ab77439cf31a4c781c77783e5ab78e
Mbugua Mwaura <mbuguamwaura9@gmail.com>
2019-05-19 18:38:44 +0300

    adds a test to check is the hero object instantiates with the age

--- src/main/java/Hero.java
+++ src/main/java/Hero.java
@@ -1,7 +1,15 @@
 public class Hero{
     private String mName;
+    private int mAge;
 
-    public Hero(String name){
+    public Hero(String name, int age){
         mName = name;
+        mAge = age;
+    }
+    public String getName(){
+        return mName;
+    }
+    public int getAge(){
+        return mAge;
     }
 }
--- src/test/java/HeroTest.java
+++ src/test/java/HeroTest.java
@@ -4,7 +4,19 @@ import static org.junit.Assert.*;
 public class HeroTest{
     @Test
     public void Hero_instantiatesCorrectly_true(){
-        Hero heroOne = new Hero("Superman");
+        Hero heroOne = new Hero("Superman",30);
         assertEquals(true, heroOne instanceof Hero);
+    }
+
+    @Test
+    public void Hero_instantiatesWithName_String(){
+        Hero heroTest = new Hero("Superman",30);
+        assertEquals("Superman", heroTest.getName());
+    }
+
+    @Test
+    public void Hero_instantiatesWithAge_Int(){
+        Hero heroTest = new Hero("Superman", 30);
+        assertEquals(30, heroTest.getAge());
     }
 }
