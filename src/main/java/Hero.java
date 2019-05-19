ed57fd8f300253a7fb54a53d473e24cf88126a94
Mbugua Mwaura <mbuguamwaura9@gmail.com>
2019-05-19 18:47:07 +0300

    adds a test to check is the hero object instantiates with the weakness

--- src/main/java/Hero.java
+++ src/main/java/Hero.java
@@ -2,11 +2,13 @@ public class Hero{
     private String mName;
     private int mAge;
     private String mPower;
+    private String mWeakeness;
 
-    public Hero(String name, int age, String power){
+    public Hero(String name, int age, String power, String weakeness){
         mName = name;
         mAge = age;
         mPower = power;
+        mWeakeness = weakeness;
     }
     public String getName(){
         return mName;
@@ -16,5 +18,8 @@ public class Hero{
     }
     public String getPower(){
         return mPower;
+    }
+    public String getWeakeness(){
+        return mWeakeness;
     }
 }
--- src/test/java/HeroTest.java
+++ src/test/java/HeroTest.java
@@ -4,25 +4,31 @@ import static org.junit.Assert.*;
 public class HeroTest{
     @Test
     public void Hero_instantiatesCorrectly_true(){
-        Hero heroOne = new Hero("Superman",30,"Flight");
+        Hero heroOne = new Hero("Superman",30,"Flight", "Kryptonite");
         assertEquals(true, heroOne instanceof Hero);
     }
 
     @Test
     public void Hero_instantiatesWithName_String(){
-        Hero heroTest = new Hero("Superman",30, "Flight");
+        Hero heroTest = new Hero("Superman",30, "Flight", "Kryptonite");
         assertEquals("Superman", heroTest.getName());
     }
 
     @Test
     public void Hero_instantiatesWithAge_Int(){
-        Hero heroTest = new Hero("Superman", 30, "Flight");
+        Hero heroTest = new Hero("Superman", 30, "Flight", "Kryptonite");
         assertEquals(30, heroTest.getAge());
     }
 
     @Test
     public void Hero_instantiatesWithPower_String(){
-        Hero heroTest = new Hero("Superman", 30, "Flight");
+        Hero heroTest = new Hero("Superman", 30, "Flight", "Kryptonite");
         assertEquals("Flight", heroTest.getPower());
+    }
+
+    @Test
+    public void Hero_instantiatesWithWeakness_String(){
+        Hero heroTest = new Hero("Supermane", 30, "Flight", "Kryptonite");
+        assertEquals("Kryptonite", heroTest.getWeakeness());
     }
 }
