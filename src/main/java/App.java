4ad846826e2c991232c7a4395f8727e9b2e8d134
Mbugua Mwaura <mbuguamwaura9@gmail.com>
2019-05-20 07:23:05 +0300

    adds the index path and page content as well as the new squad path

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -8,5 +8,24 @@ import spark.template.velocity.VelocityTemplateEngine;
 import static spark.Spark.*;
 
 public class App{
-    public static void main(String[] args){}
+    public static void main(String[] args){
+
+
+        staticFileLocation("/public");
+        String layout = "templates/layout.vtl";
+
+        get("/", (request, response) -> {
+            Map<String, Object> model = new HashMap<String, Object>();
+            model.put("template", "templates/index.vtl");
+            return new ModelAndView(model, layout);
+        }, new VelocityTemplateEngine());
+
+        get("squad/new", (request, response) -> {
+            Map<String, Object> model = new HashMap<String, Object>();
+            model.put("template", "templates/squad-form.vtl");
+            return new ModelAndView(model, layout);
+
+        }, new VelocityTemplateEngine());
+
+    }
 }
