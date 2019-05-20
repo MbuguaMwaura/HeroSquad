1f970f61cca0ba0f2982bdf1e93aa9051617081e
Mbugua Mwaura <mbuguamwaura9@gmail.com>
2019-05-20 07:39:19 +0300

    adds a squads pages to display all the squads that the user has created

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -27,5 +27,12 @@ public class App{
 
         }, new VelocityTemplateEngine());
 
+        post("/squads", (request, response) -> {
+            Map<String, Object> model = new HashMap<String, Object>();
+            model.put("squads", Squad.all());
+            model.put("template", "templates/squads.vtl");
+            return new ModelAndView(model, layout);
+        }, new VelocityTemplateEngine());
+
     }
 }
